d50c6404e8266737c9487064cab2290c16de142a
Larissa Hahn <larissaj.hahn@gmail.com>
2020-11-13 09:55:47 -0600

    Updating module

--- angular-component-library/projects/component-library/src/public-api.ts
+++ angular-component-library/projects/component-library/src/public-api.ts
@@ -2,6 +2,4 @@
  * Public API Surface of component-library
  */
 
-export * from './lib/component-library.service';
-export * from './lib/component-library.component';
-export * from './lib/component-library.module';
+export * from './lib/components/components.module';
