ec69877e8643955364780fe4f141a2d107f2c1b4
Larissa Hahn <larissaj.hahn@gmail.com>
2020-11-13 09:35:50 -0600

    Initial commit

---
+++ angular-component-library/projects/component-library/src/lib/component-library.module.ts
@@ -0,0 +1,12 @@
+import { NgModule } from '@angular/core';
+import { ComponentLibraryComponent } from './component-library.component';
+
+
+
+@NgModule({
+  declarations: [ComponentLibraryComponent],
+  imports: [
+  ],
+  exports: [ComponentLibraryComponent]
+})
+export class ComponentLibraryModule { }
