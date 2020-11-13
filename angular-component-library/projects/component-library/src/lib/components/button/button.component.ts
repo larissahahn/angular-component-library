51a7c9bb306090c635de4bf850c13c3b76b6dee3
Larissa Hahn <larissaj.hahn@gmail.com>
2020-11-13 10:35:26 -0600

    Adding setup files

--- angular-component-library/projects/component-library/src/lib/components/button/button.component.ts
+++ angular-component-library/projects/component-library/src/lib/components/button/button.component.ts
@@ -1,9 +1,20 @@
 import { Component, OnInit, EventEmitter, Output } from '@angular/core';
+import { HostBinding } from '@angular/core';
+import {
+  trigger,
+  state,
+  style,
+  animate,
+  transition
+} from '@angular/animations';
 
 @Component({
   selector: 'lib-button',
   templateUrl: './button.component.html',
-  styleUrls: ['./button.component.css']
+  styleUrls: ['./button.component.css'],
+  animations: [
+    // button animations
+  ]
 })
 export class ButtonComponent implements OnInit {
   @Output() numberOfClicks: EventEmitter<number> = new EventEmitter();
--- angular-component-library/projects/component-library/src/lib/components/components.module.ts
+++ angular-component-library/projects/component-library/src/lib/components/components.module.ts
@@ -1,12 +1,20 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { ButtonComponent } from './button/button.component';
 import { MatButtonModule } from '@angular/material/button';
+import { MatBadgeModule } from '@angular/material/badge';
 
 @NgModule({
   declarations: [ButtonComponent],
   imports: [
-    CommonModule
-  ]
+    BrowserModule,
+    BrowserAnimationsModule,
+    CommonModule,
+    MatBadgeModule,
+    MatButtonModule
+  ],
+  exports: [ButtonComponent]
 })
 export class ComponentsModule { }
